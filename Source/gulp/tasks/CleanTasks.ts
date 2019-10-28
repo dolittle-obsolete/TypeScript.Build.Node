59d8314c474010b17680eb2955271f7bab600e53
Sindre Wilting <sindrewilting@gmail.com>
2019-10-28 14:59:51 +0100

    Simplify clean tasks

--- Source/gulp/tasks/CleanTasks.ts
+++ Source/gulp/tasks/CleanTasks.ts
@@ -3,41 +3,28 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import rimraf from 'rimraf';
-import gulp from 'gulp';
 import {TaskFunction} from 'undertaker';
 import { GulpContext, createTask } from '../../internal';
 
 export class CleanTasks {
     static cleanTasks: CleanTasks
 
-    private _distributionCleanTask!: TaskFunction
-    private _typeDefsCleanTask!: TaskFunction
     private _testsCleanTask!: TaskFunction
     private _cleanTask!: TaskFunction
 
     constructor(private _context: GulpContext) {}
 
-    get distributionCleanTask() {
-        if (this._distributionCleanTask === undefined) {
-            this._distributionCleanTask = createTask(this._context, 'distribution-clean', workspace => {
+    get cleanTask() {
+        if (this._cleanTask === undefined) {
+            this._cleanTask = createTask(this._context, 'clean', workspace => {
                 let projectSources = workspace !== undefined? workspace.sources : this._context.project.sources;
                 return done => rimraf(projectSources.outputFolder!, error => done(error));
             });
         }
 
-        return this._distributionCleanTask;
+        return this._cleanTask;
     }
 
-    get typeDefsCleanTask() {
-        if (this._typeDefsCleanTask === undefined) {
-            this._typeDefsCleanTask = createTask(this._context, 'ts-clean', workspace => {
-                let projectSources = workspace !== undefined? workspace.sources : this._context.project.sources;
-                return done => projectSources.declarationFilesGlobs.forEach(glob => rimraf(glob, error => done(error)));
-            });
-        }
-    
-        return this._typeDefsCleanTask;
-    }
 
     get testsCleanTask() {
         if (this._testsCleanTask === undefined) {
@@ -49,17 +36,8 @@ export class CleanTasks {
     
         return this._testsCleanTask;
     }
-    get cleanTask() {
-        if (this._cleanTask === undefined) {
-            this._cleanTask = gulp.parallel(this.distributionCleanTask, this.typeDefsCleanTask);
-            this._cleanTask.displayName = 'clean'
-        }
-    
-        return this._cleanTask;
-    }
-
     get allTasks() {
-        return [this.cleanTask, this.testsCleanTask, this.typeDefsCleanTask, this.distributionCleanTask];
+        return [this.cleanTask, this.testsCleanTask];
     }
 }
 
